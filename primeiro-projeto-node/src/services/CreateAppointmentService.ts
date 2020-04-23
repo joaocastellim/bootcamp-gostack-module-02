37ad67f60809af77268cdf78c5b84a96f1a8cf1c
João Castelli <joaao.castelli@gmail.com>
2020-04-23 19:24:55 -0300

    updates

--- primeiro-projeto-node/src/services/CreateAppointmentService.ts
+++ primeiro-projeto-node/src/services/CreateAppointmentService.ts
@@ -1,35 +1,36 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import Appointment from '../models/Appointment';
+import AppError from '../errors/AppError';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppoimentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppoimentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
     if (findAppoimentInSameDate) {
-      throw Error('This appointment is alredy booked');
+      throw new AppError('This appointment is alredy booked');
     }
 
-    const appointment = this.appointmentsRepository.create({
-      provider,
+    const appointment = appointmentsRepository.create({
+      provider_id,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
+
     return appointment;
   }
 }
